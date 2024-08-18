400cfc5fd4a7cb651f5854e61a1bf6e7b95afc72
sydeveloperslk <sydeveloperslk@gmail.com>
2024-08-18 20:41:07 +0530

    s

--- index.js
+++ index.js
@@ -32,6 +32,6 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('A Monqqqk in Cloud'); //write a response to the client
+  res.write('A Cloud'); //write a response to the client
   res.end(); //end the response
 }).listen(80); //the server object listens on port 80
