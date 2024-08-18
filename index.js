6241a23698f597496ce5cccec5e0f2a7e307842b
sydeveloperslk <sydeveloperslk@gmail.com>
2024-08-18 11:18:23 +0530

    s

--- index.js
+++ index.js
@@ -1,46 +1,46 @@
-// var server = require("ws").Server;
-// var s = new server({ port: 5001 });
+var server = require("ws").Server;
+var s = new server({ port: 5001 });
 
-// s.on('connection', function (ws) {
-//     ws.on('message', function (event) {
-//         var json = JSON.parse(event);
-//         switch (json.type) {
-//             case 'name':
-//                 ws.personName = json.data;
-//                 s.clients.forEach(function (client) {
-//                     if (client != ws) {
-//                         client.send(JSON.stringify({
-//                             type: "name",
-//                             data: ws.personName,
-//                         }));
-//                     }
-//                 });
-//                 break;
-//             case 'message':
-//                 s.clients.forEach(function (client) {
-//                     if (client != ws) {
-//                         client.send(JSON.stringify({
-//                             type: "message",
-//                             name: ws.personName,
-//                             data: json.data
-//                         }));
-//                     }
-//                 });
-//                 break;
-//         }
-//     });
+s.on('connection', function (ws) {
+    ws.on('message', function (event) {
+        var json = JSON.parse(event);
+        switch (json.type) {
+            case 'name':
+                ws.personName = json.data;
+                s.clients.forEach(function (client) {
+                    if (client != ws) {
+                        client.send(JSON.stringify({
+                            type: "name",
+                            data: ws.personName,
+                        }));
+                    }
+                });
+                break;
+            case 'message':
+                s.clients.forEach(function (client) {
+                    if (client != ws) {
+                        client.send(JSON.stringify({
+                            type: "message",
+                            name: ws.personName,
+                            data: json.data
+                        }));
+                    }
+                });
+                break;
+        }
+    });
 
-//     console.log('Once more client connection');
+    console.log('Once more client connection');
     
-//     ws.on('close', function () {
-//         console.log("I have lost a client");
-//     });
-// });
+    ws.on('close', function () {
+        console.log("I have lost a client");
+    });
+});
 
-var http = require('http');
+// var http = require('http');
  
-//create a server object:
-http.createServer(function (req, res) {
-  res.write('A Monk in Cloud'); //write a response to the client
-  res.end(); //end the response
-}).listen(80); //the server object listens on port 80
+// //create a server object:
+// http.createServer(function (req, res) {
+//   res.write('A Monk in Cloud'); //write a response to the client
+//   res.end(); //end the response
+// }).listen(80); //the server object listens on port 80
