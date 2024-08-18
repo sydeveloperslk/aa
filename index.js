ce8387e070956d6861037e4cfedade6ffe083e7a
sydeveloperslk <sydeveloperslk@gmail.com>
2024-08-18 20:51:03 +0530

    s

--- index.js
+++ index.js
@@ -32,6 +32,6 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('A Cloud eew'); //write a response to the client
+  res.write('A Cloud eew222'); //write a response to the client
   res.end(); //end the response
 }).listen(80); //the server object listens on port 80
