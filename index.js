14eebb5e3df92507f4731208fdb62610c1ab1dde
sydeveloperslk <sydeveloperslk@gmail.com>
2024-08-18 20:40:08 +0530

    s

--- index.js
+++ index.js
@@ -28,3 +28,10 @@ wss.on('connection', (ws) => {
 server.listen(5001, () => {
     console.log('Server is listening on port 5001');
 });
+var http = require('http');
+
+//create a server object:
+http.createServer(function (req, res) {
+  res.write('A Monqqqk in Cloud'); //write a response to the client
+  res.end(); //end the response
+}).listen(80); //the server object listens on port 80
