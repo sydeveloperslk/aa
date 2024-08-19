7a5e6e2775122e56b39cd50fc95d3f1a06b82c57
sydeveloperslk <sydeveloperslk@gmail.com>
2024-08-19 22:32:19 +0530

    s

--- index.js
+++ index.js
@@ -37,10 +37,10 @@ s.on('connection', function (ws) {
     });
 });
 
-// var http = require('http');
+var http = require('http');
  
-// //create a server object:
-// http.createServer(function (req, res) {
-//   res.write('A Monk in Cloud'); //write a response to the client
-//   res.end(); //end the response
-// }).listen(80); //the server object listens on port 80
+//create a server object:
+http.createServer(function (req, res) {
+  res.write('A Monk in Cloud'); //write a response to the client
+  res.end(); //end the response
+}).listen(80); //the server object listens on port 80
