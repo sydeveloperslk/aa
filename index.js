84e31b0c120f9b2f86cf478b4b014f216c38699d
sydeveloperslk <sydeveloperslk@gmail.com>
2024-08-19 23:03:47 +0530

    s

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
+var https = require('https');
+var fs = require('fs');
 var server = require("ws").Server;
+var options = {
+    key: fs.readFileSync('cert/key.pem'),
+    cert: fs.readFileSync('cert/cert.pem')
+  };
+  
 var s = new server({ port: 5001 });
+ 
+var httpsServer = https.createServer(options, function (req, res) {
+    res.write('A Monk in Cloud2'); //write a response to the client
+    res.end(); //end the response
+  }).listen(443); //HTTPS typically uses port 443
+  var s = new server({ server: httpsServer });
 
 s.on('connection', function (ws) {
     ws.on('message', function (event) {
@@ -36,11 +49,3 @@ s.on('connection', function (ws) {
         console.log("I have lost a client");
     });
 });
-
-var http = require('http');
- 
-//create a server object:
-http.createServer(function (req, res) {
-  res.write('A Monk in Cloud2 '); //write a response to the client
-  res.end(); //end the response
-}).listen(80); //the server object listens on port 80
